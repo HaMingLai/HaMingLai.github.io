5e35b72702aacbe72dcad7b76a057f8ba8ff2cf7
HaMingLai <526003748@qq.com>
2016-03-03 16:03:40 +0800

    Update support2048.js

--- my2048/support2048.js
+++ my2048/support2048.js
@@ -1,4 +1,4 @@
-documentWidth = window.screen.width;
+documentWidth = $(window).width();
 gridContainerWidth = 0.92 * documentWidth;
 cellSideLength = 0.18 * documentWidth;
 cellSpace = 0.04 * documentWidth;
